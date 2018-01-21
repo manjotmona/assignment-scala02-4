1317d48064ccd0dc2fa7206c2b9955d5d599be50
Manjot Kaur <manjotmona@gmail.com>
2018-01-21 20:09:59 +0530

    Changes done by adding an implicit class within scope.

--- src/main/scala/edu/knoldus/Commission.scala
+++ src/main/scala/edu/knoldus/Commission.scala
@@ -10,38 +10,51 @@ case class ClientSideCommission(value: Int) extends Commission(value: Int)
 
 case class StreetSideCommission(value: Int) extends Commission(value: Int)
 
-private sealed trait CommissionDisplay {
+sealed trait CommissionDisplay {
   def totalDisplayCommission: String
 }
 
-import scala.reflect.runtime.universe._
+class TotalCommission[T] {
 
-private class TotalCommission[T: TypeTag](GenericList: List[T]) extends CommissionDisplay {
-  def totalDisplayCommission: String = {
-    def internalSumFunc(commissionList: List[T],
-        sum: Int, client: Boolean, street: Boolean): String = {
-      commissionList match {
-        case Nil => methodPrint(sum, client, street)
-        case head :: tail if head.isInstanceOf[ClientSideCommission] =>
-          val clientTempInstance = head.asInstanceOf[ClientSideCommission]
-          internalSumFunc(tail, sum + clientTempInstance.value, true, street)
-        case head :: tail if head.isInstanceOf[StreetSideCommission] =>
-          val streetTempInstance = head.asInstanceOf[StreetSideCommission]
-          internalSumFunc(tail, sum + streetTempInstance.value, client, true)
-        case _ => "No Matching"
+  import TotalCommission._
+
+  def getTotalCommission(list: List[T]): String = {
+    list.totalDisplayCommission
+  }
+
+}
+
+object TotalCommission {
+
+  implicit class TotalCommissionEx[T](val genericList: List[T]) extends CommissionDisplay {
+    def totalDisplayCommission: String = {
+      def internalSumFunc(commissionList: List[T],
+          sum: Int, client: Boolean, street: Boolean): String = {
+        commissionList match {
+          case Nil => methodPrint(sum, client, street)
+          case head :: tail if head.isInstanceOf[ClientSideCommission] =>
+            val clientTempInstance = head.asInstanceOf[ClientSideCommission]
+            internalSumFunc(tail, sum + clientTempInstance.value, true, street)
+          case head :: tail if head.isInstanceOf[StreetSideCommission] =>
+            val streetTempInstance = head.asInstanceOf[StreetSideCommission]
+            internalSumFunc(tail, sum + streetTempInstance.value, client, true)
+          case _ => "No Matching"
+        }
+      }
+
+      internalSumFunc(genericList, 0, false, false)
+    }
+
+    def methodPrint(sum: Int, client: Boolean, street: Boolean): String = {
+      (client, street) match {
+        case (true, false) => "The total client commission is  " + sum
+        case (false, true) => "The total street commission is  " + sum
+        case (true, true) => "The total mixed commission is  " + sum
+        case (false, false) => "There is no commission."
       }
     }
-    internalSumFunc(GenericList, 0, false, false)
   }
 
-  def methodPrint(sum: Int, client: Boolean, street: Boolean): String = {
-    (client, street) match {
-      case (true, false) => "The total client commission is  " + sum
-      case (false, true) => "The total street commission is  " + sum
-      case (true,true) => "The total mixed commission is  " + sum
-      case (false,false) => "There is no commission."
-    }
-  }
 }
 
 private object Application extends App {
@@ -50,8 +63,9 @@ private object Application extends App {
   val thirdClient = new ClientSideCommission(2)
   val firstStreet = new StreetSideCommission(2)
   val secondStreet = new StreetSideCommission(2)
-  val totalCommission = new TotalCommission[Commission](List(firstClient,firstStreet,secondClient,secondStreet,thirdClient))
-  print(totalCommission.totalDisplayCommission)
+  val totalCommission = new TotalCommission[Commission]
+  print(totalCommission
+    .getTotalCommission(List(firstClient, firstStreet, secondClient, secondStreet, thirdClient)))
 }
 
 
